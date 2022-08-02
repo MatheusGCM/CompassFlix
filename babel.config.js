7dd067d63a246ca4fd1843e03b784e4804f10abb
MichelRibeiro48 <michel-amoral@hotmail.com>
2022-08-02 11:32:26 -0300

    Adicionado Fonts e Estilização Login

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  // dependencies: {
-  //   'react-native-vector-icons': {
-  //     platforms: {
-  //       ios: null,
-  //     },
-  //   },
-  // },
   presets: ['module:metro-react-native-babel-preset'],
 };
--- src/Components/EmailPasswordField/index.js
+++ src/Components/EmailPasswordField/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Text, TextInput, View} from 'react-native';
 import styles from './style';
+import IconLock from 'react-native-vector-icons/EvilIcons';
 
-export const EmailPasswordField = ({isPassword, inputName}) => {
+export const EmailPasswordField = ({isPassword, inputName, iconName}) => {
   return (
     <View style={styles.boxInput}>
+      <IconLock
+        size={25}
+        name={iconName}
+        color={'#ffffff80'}
+        style={{paddingTop: 8, paddingLeft: 10}}
+      />
       <TextInput
         style={styles.input}
         placeholder={inputName}
