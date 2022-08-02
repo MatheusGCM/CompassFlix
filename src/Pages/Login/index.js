f28df3c366eb1a33338eabf00b959e609e1bd684
MatheusGCM <nexteu10@outlook.com>
2022-08-02 20:35:33 -0300

    Adicionando o State

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import {EmailPasswordField} from '../../Components/EmailPasswordField';
 import styles from './style';
 
 const Login = ({navigation}) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <View style={styles.page}>
       <Text style={styles.loginText}>Login</Text>
@@ -12,12 +15,16 @@ const Login = ({navigation}) => {
       </Text>
       <View style={styles.input}>
         <EmailPasswordField
+          value={email}
+          setValue={setEmail}
           isPassword={false}
           inputName={'e-mail'}
           iconName={'user'}
         />
       </View>
       <EmailPasswordField
+        value={password}
+        setValue={setPassword}
         isPassword={true}
         inputName={'senha'}
         iconName={'lock'}
