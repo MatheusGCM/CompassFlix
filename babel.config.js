a60b1753d70906da6e9c5176f1e18fdb8111e558
MichelRibeiro48 <michel-amoral@hotmail.com>
2022-08-01 23:58:46 -0300

    Comentado depencias do babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  // dependencies: {
+  //   'react-native-vector-icons': {
+  //     platforms: {
+  //       ios: null,
+  //     },
+  //   },
+  // },
   presets: ['module:metro-react-native-babel-preset'],
 };
