85c20e3ffaab04deb3f2b1289c4b8a66a611e655
MichelRibeiro48 <michel-amoral@hotmail.com>
2022-08-31 17:16:59 -0300

    Estilizando

--- src/Components/ListComponent/index.js
+++ src/Components/ListComponent/index.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from 'react';
-import {FlatList, Text, TouchableOpacity, View} from 'react-native';
+import {Dimensions, FlatList, Text, TouchableOpacity, View} from 'react-native';
 import IconTrash from 'react-native-vector-icons/EvilIcons';
 import {Context} from '../../context';
 import {createListMovies, deleteList} from '../../service/api';
@@ -45,59 +45,20 @@ export default function ListComponent(data) {
             : null
         }
         renderItem={({item}) => (
-          <View
-            style={{
-              width: '100%',
-              flexDirection: 'row',
-              paddingBottom: 16,
-            }}>
+          <View style={style.flatListStyle}>
             <TouchableOpacity
               onPress={() =>
                 navigation.navigate('ListFilmPage', {list_id: item.id})
               }
-              style={{
-                backgroundColor: '#8F9AFC',
-                height: 100,
-                width: '88%',
-                flexGrow: 0,
-                paddingLeft: 37,
-                paddingVertical: 20,
-                borderBottomLeftRadius: 10,
-                borderTopLeftRadius: 10,
-                borderColor: '#E9A6A6',
-              }}>
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 14,
-                  width: 120,
-                  fontFamily: 'OpenSans-Medium',
-                }}>
-                {item.name}
-              </Text>
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 10,
-                  paddingTop: 14,
-                  fontFamily: 'OpenSans-Bold',
-                }}>
-                {item.item_count} filmes
-              </Text>
+              style={style.buttonList}>
+              <Text style={style.textNameList}>{item.name}</Text>
+              <Text style={style.textMovies}>{item.item_count} filmes</Text>
             </TouchableOpacity>
             <TouchableOpacity
               onPress={() => {
                 setModalExit(!modalExit), setDeleteId(item.id);
               }}
-              style={{
-                backgroundColor: '#E9A6A6',
-                borderColor: '#E9A6A6',
-                width: 40,
-                borderBottomRightRadius: 10,
-                borderTopRightRadius: 10,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+              style={style.buttonTrash}>
               <IconTrash name="trash" size={28} color="#EC26269C" />
             </TouchableOpacity>
             <ModalExit
@@ -124,15 +85,7 @@ export default function ListComponent(data) {
       />
       {data.data.length <= 3 && !modalAddList ? (
         <TouchableOpacity
-          style={{
-            display: 'flex',
-            backgroundColor: 'pink',
-            alignSelf: 'flex-end',
-            padding: 10,
-            marginBottom: 52,
-            marginRight: 18,
-            borderRadius: 100,
-          }}
+          style={style.buttonPlusResponsive}
           onPress={() => setModalAddList(!modalAddList)}>
           <PlusIcon name="plus" size={28} color="black" />
         </TouchableOpacity>
