04cfe28f742430bc4ae08cfb6e2977ca39f5a6e4
MatheusGCM <nexteu10@outlook.com>
2022-09-08 06:29:05 -0300

    Greeting component coverage test

--- src/Components/Greeting/index.js
+++ src/Components/Greeting/index.js
@@ -4,36 +4,26 @@ import {Text, View} from 'react-native';
 import styles from './style';
 
 const Greeting = ({screen, user}) => {
-  return screen === 'HomeMovie' ? (
+  return (
     <>
       <View style={styles.row}>
         <Text style={styles.header_title}>Olá,</Text>
-        <Text style={styles.header_label}>{user.name}</Text>
+        <Text style={styles.header_label}>
+          {user.name ? user.name : user.username}
+        </Text>
         <Text style={styles.header_text}>!</Text>
       </View>
       <View style={styles.marginBottom}>
         <Text style={styles.header_description}>
-          Reveja ou acompanhe os filmes que você assistiu...
+          Reveja ou acompanhe{' '}
+          {screen === 'HomeMovie' ? 'os filmes' : 'as séries'} que você
+          assistiu...
         </Text>
       </View>
       <View>
-        <Text style={styles.title}>Filmes populares este mês</Text>
-      </View>
-    </>
-  ) : (
-    <>
-      <View style={styles.row}>
-        <Text style={styles.header_title}>Olá,</Text>
-        <Text style={styles.header_label}>{user.name}</Text>
-        <Text style={styles.header_text}>!</Text>
-      </View>
-      <View style={styles.marginBottom}>
-        <Text style={styles.header_description}>
-          Reveja ou acompanhe as séries que você assistiu...
+        <Text style={styles.title}>
+          {screen === 'HomeMovie' ? 'Filmes' : 'Séries'} populares este mês
         </Text>
-      </View>
-      <View>
-        <Text style={styles.title}>Séries populares este mês</Text>
       </View>
     </>
   );
